d7b19a35ddcf568ae93f3978b5fa3494cbfaadd5
RubyFore <ruby.a.fore@gmail.com>
2019-02-27 10:38:29 -0500

    chaning RR for UnequalSetsBurden

--- RunningRefactoredCode/BurdenUnequalSetsVaryPercentCausal.sh
+++ RunningRefactoredCode/BurdenUnequalSetsVaryPercentCausal.sh
@@ -12,12 +12,12 @@ source('~/Pathways2019/RunningRefactoredCode/runSimsBeaker.R')
 # adjusting the percent causal in unequal set sizes, 1/8 vs 7/8
 
 #define parameter frame
-rr_var <- list(c(rep(1,32), rep(1,192), rep(1.25,32)), 
-               c(rep(1.25,4), rep(1, 28),  rep(1,196),rep(1.25, 28)),
-               c(rep(1.25, 8), rep(1,24), rep(1,200), rep(1.25, 24)), 
-               c(rep(1.25,12), rep(1,20), rep(1,204), rep(1.25, 20)), 
-               c(rep(1.25, 20), rep(1,12), rep(1,212), rep(1.25, 12)), 
-               c(rep(1.25, 32), rep(1, 224)))
+rr_var <- list(c(rep(1,32), rep(1,192), rep(1.15,32)), 
+               c(rep(1.15,4), rep(1, 28),  rep(1,196),rep(1.15, 28)),
+               c(rep(1.15, 8), rep(1,24), rep(1,200), rep(1.15, 24)), 
+               c(rep(1.15,12), rep(1,20), rep(1,204), rep(1.15, 20)), 
+               c(rep(1.15, 20), rep(1,12), rep(1,212), rep(1.15, 12)), 
+               c(rep(1.15, 32), rep(1, 224)))
 set_sizes <- list(c(32, 224))
 n = 1000
 maf = .05
